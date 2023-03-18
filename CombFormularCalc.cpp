3228a27cccf948c93607aaa088e9075def0ca1ba
TimoshaMorozov <morozovtd1910@gmail.com>
2023-03-18 12:00:58 +0300

    Update CombFormularCalc.cpp

--- CombFormularCalc.cpp
+++ CombFormularCalc.cpp
@@ -31,5 +31,33 @@ void PlaceWithoutRepet()
 	cout << endl;
 	result = fact(placement) / fact(placement-positions);
 	cout << result;
-
 };
+void CombWithRepet()
+{
+	double n, m, result;
+	cin >> n;
+	cout << endl;
+	cin >> m;
+	cout << endl;
+	result = (fact(n+m-1))/(fact(m)*fact(n-1));
+	cout << result;
+};
+void CombWithoutRepet()
+{
+	double n, m, result;
+	cin >> n;
+	cout << endl;
+	cin >> m;
+	cout << endl;
+	result = fact(n)/(fact(n-m)*fact(m));
+	cout << result;
+};
+void Permutations()
+{
+	double permutations, result;
+	cin >> permutations;
+	cout << endl;
+	result = fact(permutations);
+	cout << result;
+}
+

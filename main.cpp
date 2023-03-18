304a77c2e62db2123b30180c61ac12ed6245a009
sonicreverb <110459178+sonicreverb@users.noreply.github.com>
2023-03-18 10:28:12 +0300

    Init Commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+// переменные counter_x
+// функций CamelCase CountSomeWoods(); InpMatrixArr();
+// структуры struct CamelCase MatrixArr;
+// int return -1
+// ввод и вывод внутри функций
+// обработка ошибок
+// Пример ввода - "Введите ваше значение: "; "Введённое значение некорректно. Повторите ввод: "
+
+int main()
+{
+	return 0;
+}
